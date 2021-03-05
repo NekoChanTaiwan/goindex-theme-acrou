b99142563ff1f78883998ca820d14ef786f38aa1
NekoChan <aaron74327@gmail.com>
2021-03-05 01:57:38 -0800

    CN

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+var scripts = ["https://cdn.jsdelivr.net/gh/NekoChanTaiwan/goindex-theme-acrou@v2.0.8.cn.4/dist/js/app.631e2a56.js","https://cdn.jsdelivr.net/gh/NekoChanTaiwan/goindex-theme-acrou@v2.0.8.cn.4/dist/js/chunk-vendors.6df9f5f6.js"];
+var cdnjs = ["https://cdn.bootcdn.net/ajax/libs/vue/2.6.11/vue.min.js","https://cdn.bootcdn.net/ajax/libs/vue-i18n/8.17.3/vue-i18n.min.js","https://cdn.bootcdn.net/ajax/libs/vue-router/3.1.6/vue-router.min.js","https://cdn.bootcdn.net/ajax/libs/vuex/3.5.1/vuex.js","https://cdn.bootcdn.net/ajax/libs/axios/0.19.2/axios.min.js","https://cdn.bootcdn.net/ajax/libs/element-ui/2.13.1/index.js","https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.15/lodash.min.js","https://cdn.bootcdn.net/ajax/libs/js-cookie/2.2.1/js.cookie.min.js","https://cdn.bootcdn.net/ajax/libs/lowdb/1.0.0/low.min.js","https://cdn.jsdelivr.net/npm/lowdb@1.0.0/dist/LocalStorage.min.js","https://at.alicdn.com/t/font_1760192_8add3xcxs4u.js"];
+cdnjs.forEach((item) => {
+  document.write('<script src="' + item + '"></script>');
+});
+scripts.forEach((item) => {
+  document.write('<script src="' + item + '"></script>');
+});
