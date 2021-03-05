10afdce260c453a8c3c8c4fd90426df10e4dadb5
NekoChan <aaron74327@gmail.com>
2021-03-05 01:57:05 -0800

    clean

--- dependencies-cdn.js
+++ dependencies-cdn.js
@@ -23,7 +23,7 @@ module.exports = [
   { name: 'lodash', library: '_', js: 'https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.15/lodash.min.js', css: '' },
   { name: 'js-cookie', library: 'Cookies', js: 'https://cdn.bootcdn.net/ajax/libs/js-cookie/2.2.1/js.cookie.min.js', css: '' },
   { name: 'lowdb', library: 'low', js: 'https://cdn.bootcdn.net/ajax/libs/lowdb/1.0.0/low.min.js', css: '' },
-  { name: 'lowdb/adapters/LocalStorage', library: 'LocalStorage', js: 'https://cdn.bootcdn.net/ajax/libs/lowdb/1.0.0/low.min.js', css: '' },
+  { name: 'lowdb/adapters/LocalStorage', library: 'LocalStorage', js: 'https://cdn.jsdelivr.net/npm/lowdb@1.0.0/dist/LocalStorage.min.js', css: '' }, // bootcdn 的版本和 jsdelivr不同
   { name: '', library: '', js: 'https://at.alicdn.com/t/font_1760192_8add3xcxs4u.js', css: '' },
   { name: '', library: '', js: '', css: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.2/css/fontawesome.min.css' },
   { name: '', library: '', js: '', css: 'https://cdn.bootcdn.net/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.min.css' },
